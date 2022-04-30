0a916d7a655e2fa04cec7e002556d8ed51f12710
Hao-Jen Wang <ljf00862@gmail.com>
2022-04-30 18:59:44 +0000

    refactor simclr.py + add simclr_audio.py

--- simclr_audio.py
+++ simclr_audio.py
@@ -2,7 +2,8 @@ import os
 import numpy as np
 import torch
 import torch.nn as nn
-import torchvision
+import torchvision, torchaudio
+import torch_audiomentations
 import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
@@ -13,14 +14,12 @@ from torch.utils.data import Dataset, DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 from pytorch_lightning.trainer.trainer import Trainer
-from pl_bolts.datamodules import CIFAR10DataModule
-from pl_bolts.datamodules.imagenet_datamodule import ImagenetDataModule
-from pl_bolts.transforms.dataset_normalizations import cifar10_normalization, imagenet_normalization
 from pl_bolts.models.self_supervised import SimCLR
 from pl_bolts.models.self_supervised.moco.transforms import GaussianBlur
 from pl_bolts.models.self_supervised.resnets import resnet50
 
-class ImageDataTransform:
+
+class AudioDataTransform:
     """
     Transform used for DataLoader
     output_length parameter depends on what model to use
@@ -29,20 +28,18 @@ class ImageDataTransform:
     def __init__(self, height=32, mode="train", output_length=3):
         if mode == "train":
             self.transforms = transforms.Compose([
-                transforms.RandomResizedCrop(height, scale=(0.2, 1.0)),
-                transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),  # not strengthened
-                transforms.RandomGrayscale(p=0.2),
-                transforms.RandomApply([GaussianBlur([0.1, 2.0])], p=0.5),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                cifar10_normalization(),
+                torch_audiomentations.Gain(),
+                torch_audiomentations.PolarityInversion(),
+                torch_audiomentations.AddColoredNoise(max_snr_in_db=15, sample_rate=16000),
+                torchaudio.transforms.Spectrogram(normalized=True),
+                torchaudio.transforms.AmplitudeToDB(), # if not used, spectrogram becomes unreadable
+                torchvision.transforms.RandomCrop(size=(201, 116)), # minimum size
             ])
         else: # val or test
             self.transforms = transforms.Compose([
-                transforms.Resize(height + 12),
-                transforms.CenterCrop(height),
-                transforms.ToTensor(),
-                cifar10_normalization(),
+                torchaudio.transforms.Spectrogram(normalized=True),
+                torchaudio.transforms.AmplitudeToDB(), # if not used, spectrogram becomes unreadable
+                torchvision.transforms.RandomCrop(size=(201, 116)), # minimum size
             ])
         
         self.output_length = output_length
@@ -55,35 +52,50 @@ class ImageDataTransform:
         # output_length=3 is for SimCLR pre-training
         return [self.transforms(sample) for _ in range(self.output_length)]
 
+class LibrispeechDataset(Dataset):
+    """
+    Create train/val/test split for Librispeech Dataset
+    train: 2303, val: 200, test: 200, num_labels: 40
+    """
+    def __init__(self, mode="train", transform=None, path="./librispeech"):
+        # get data from path
+        dataset = torchaudio.datasets.LIBRISPEECH("./librispeech", url="dev-clean", download=True)
+        ids, ys = list(range(len(dataset))), [speaker_id for _, _, _, speaker_id, _, _ in dataset]
 
-class CIFAR10Dataset(Dataset):
-    """
-    Create train/val/test split for CIFAR10 Dataset
-    """
-    def __init__(self, mode="train", transform=None):
-        (X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
-        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, 
-                    test_size=5000, random_state=0, stratify=y_train)
+        # train/val/test split
+        id_train, id_test, y_train, y_test = train_test_split(ids, ys, test_size=200, random_state=0, stratify=ys)
+        id_train, id_val, y_train, y_val = train_test_split(id_train, y_train, test_size=200, random_state=0, stratify=y_train)
         
-        if mode == "train":
-            self.images, self.labels = X_train, y_train
-        elif mode == "val":
-            self.images, self.labels = X_val, y_val
-        else:
-            self.images, self.labels = X_test, y_test
-
+        # attributes
+        self.mode = mode
+        self.dataset = dataset
         self.transform = transform
+        self.id_train, self.id_val, self.id_test = id_train, id_val, id_test
+        self.id_to_label = {y: i for i, y in enumerate(set(ys))} # for training indexing
 
     def __len__(self):
-        return len(self.labels)
+        if self.mode == "train":
+            return len(self.id_train)
+        return len(self.id_val) if self.mode == "val" else len(self.id_test)
 
     def __getitem__(self, idx):
-        image, label = self.images[idx], self.labels[idx]
-        image = Image.fromarray(image) # for torchvision.transforms.ToTensor()
-        if self.transform: # note: ImageDataTransform will produce a tuple
-            image = self.transform(image) 
+        if self.mode == "train":
+            new_idx = self.id_train[idx]
+        else:
+            new_idx = self.id_val[idx] if self.mode == "val" else self.id_test[idx]
+
+        audio, _, _, speaker_id, _, _ = self.dataset[new_idx]
+        label = self.id_to_label[speaker_id]
+
+        if self.transform:
+            audio = audio.unsqueeze(0) # required channel dimenstion for torch_audiomentations
+            audio = self.transform(audio)
+            if type(audio) == list: # when transform's output_length > 1
+                audio = [w.squeeze(0) for w in audio]
+            else:
+                audio = audio.squeeze(0) # change back
         
-        return image, label
+        return audio, label
 
 class SimCLR_finetuned(nn.Module):
     """
@@ -99,6 +111,7 @@ class SimCLR_finetuned(nn.Module):
             self.backbone.requires_grad_(False)
         else: # just want to get encoder, parameters in SimCLR are not used
             self.backbone = resnet50()
+            self.backbone.conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False) # mono-chn
 
         self.linear_layer = nn.Sequential(
             nn.Linear(hidden_dim, num_classes),
@@ -111,27 +124,28 @@ class SimCLR_finetuned(nn.Module):
         return out
 
 # 1st Stage
-def cifar10_pretrain(num_epochs=300, batch_size=256):
+def librispeech_pretrain(num_epochs=1, batch_size=64):
     """
     Self-supervised pretraining, will not use test set
     """
 
     # Data Loader
-    train_set = CIFAR10Dataset(mode="train", transform=ImageDataTransform(mode="train"))
-    val_set = CIFAR10Dataset(mode="val", transform=ImageDataTransform(mode="val"))
+    train_set = LibrispeechDataset(mode="train", transform=AudioDataTransform(mode="train"))
+    val_set = LibrispeechDataset(mode="val", transform=AudioDataTransform(mode="val"))
 
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=6)
     val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=6)
 
     # Model
-    model = SimCLR(num_samples=40000, batch_size=batch_size, dataset="cifar10", gpus=1)
+    model = SimCLR(num_samples=len(train_set), batch_size=batch_size, dataset="", gpus=1)
+    model.encoder.conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False) # mono-chn
 
     # Fit
     trainer = Trainer(gpus=1, max_epochs=num_epochs)
     trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
 # 2nd Stage
-def cifar10_finetune(weight_path=None, num_epochs=100, batch_size=64, log_file="./log_finetune", lr=0.001):
+def librispeech_finetune(weight_path=None, num_epochs=100, batch_size=64, log_file="./log_finetune", lr=0.001):
     """
     Hyper-parameters should be fixed across different self-supervised models
     ref: https://pytorch-lightning-bolts.readthedocs.io/en/latest/self_supervised_models.html
@@ -140,13 +154,19 @@ def cifar10_finetune(weight_path=None, num_epochs=100, batch_size=64, log_file="
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # Load pre-trained model
-    model = SimCLR.load_from_checkpoint(weight_path, strict=False) if weight_path else None
-    model = SimCLR_finetuned(pretrained_model=model).to(device)
+    if weight_path:
+        model = SimCLR(num_samples=-1, batch_size=-1, dataset="", gpus=1)
+        model.encoder.conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False) # mono-chn
+        model.load_state_dict(torch.load(weight_path), strict=False)
+    else:
+        model = None
+
+    model = SimCLR_finetuned(pretrained_model=model, num_classes=40).to(device)
 
     # Data Loader
-    train_set = CIFAR10Dataset(mode="train", transform=ImageDataTransform(mode="train", output_length=1))
-    val_set = CIFAR10Dataset(mode="val", transform=ImageDataTransform(mode="val", output_length=1))
-    test_set = CIFAR10Dataset(mode="test", transform=ImageDataTransform(mode="test", output_length=1))
+    train_set = LibrispeechDataset(mode="train", transform=AudioDataTransform(mode="train", output_length=1))
+    val_set = LibrispeechDataset(mode="val", transform=AudioDataTransform(mode="val", output_length=1))
+    test_set = LibrispeechDataset(mode="test", transform=AudioDataTransform(mode="test", output_length=1))
 
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=6)
     val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=6)
@@ -224,7 +244,7 @@ def cifar10_finetune(weight_path=None, num_epochs=100, batch_size=64, log_file="
     # Test
     if not best_model_path:
         return
-    best_model = SimCLR_finetuned().to(device)
+    best_model = SimCLR_finetuned(num_classes=40).to(device)
     best_model.load_state_dict(torch.load(best_model_path), strict=False)
 
     accuracy = 0.0
